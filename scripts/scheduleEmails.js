90c5fba105978c6bb4a153356b1f83fc7cdfcfc4
Aman Singh <amansingh080704@gmail.com>
2025-02-16 08:45:25 +0530

    monthly mail

--- scripts/scheduleEmails.js
+++ scripts/scheduleEmails.js
@@ -1,12 +1,12 @@
 const cron = require('node-cron');
 const sendMonthlyReport = require('../utils/sendEmail');
-const { getUserData } = require('../utils/userData'); // Assume this function fetches user data
+const { getUserData } = require('../utils/userData');
 
 cron.schedule('0 0 1 * *', async () => {
   const users = await getUserData(); // Fetch user data from your database
 
   for (const user of users) {
-    const { email, expensesData, incomeData } = user;
-    await sendMonthlyReport(email, expensesData, incomeData);
+    const { email, expensesSummary, incomeSummary } = user;
+    await sendMonthlyReport(email, expensesSummary, incomeSummary);
   }
 });
--- utils/userData.js
+++ utils/userData.js
@@ -9,13 +9,16 @@ async function getUserData() {
   });
 
   return users.map(user => {
-    const expensesData = user.transactions.filter(t => t.type === 'EXPENSE');
-    const incomeData = user.transactions.filter(t => t.type === 'INCOME');
+    const expenses = user.transactions.filter(t => t.type === 'EXPENSE');
+    const income = user.transactions.filter(t => t.type === 'INCOME');
+
+    const expensesSummary = expenses.reduce((sum, t) => sum + parseFloat(t.amount), 0).toFixed(2);
+    const incomeSummary = income.reduce((sum, t) => sum + parseFloat(t.amount), 0).toFixed(2);
 
     return {
       email: user.email,
-      expensesData,
-      incomeData,
+      expensesSummary,
+      incomeSummary,
     };
   });
 }
